d554e8a6cb25c072217b2f386552d694b94e2d89
Edenerus <112581029+Edenerus@users.noreply.github.com>
2022-09-22 23:36:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,9 @@ app.register_blueprint(main_blueprint)
 app.register_blueprint(loader_blueprint)
 
 
-
-
 @app.route("/uploads/<path:path>")
 def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-
 app.run(debug=True)
-
